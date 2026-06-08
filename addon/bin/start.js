0be6b65187300c7c4d2119305e8e5d4a750ebee5
James <jamessundaram8@gmail.com>
2026-06-08 13:38:07 +0100

    feat(addon): init downloads on startup

--- addon/bin/start.js
+++ addon/bin/start.js
@@ -1,7 +1,11 @@
 import { createServer } from '../src/server.js';
 import { config } from '../src/config.js';
+import { initDownloads } from '../src/downloader.js';
 
 const [host, port] = config.bind.split(':');
+
+await initDownloads(config.downloadDir);
+
 createServer().listen(Number(port), host, () => {
   console.log(`addon listening on http://${host}:${port}`);
 });
